1195e8d1852e72c36b830bba919c3e2e852b2155
Hiraku <dev@hiraku.tw>
2022-06-09 14:23:18 +0800

    modify zshrc

--- scripts/zim/zshrc.sh
+++ scripts/zim/zshrc.sh
@@ -3,17 +3,6 @@
 echo '
 POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
 
-# You may need to manually set your language environment
-export LANG="en_US.UTF-8"
-export LANGUAGE="en_US.UTF-8"
-export LC_COLLATE="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-export LC_MESSAGES="en_US.UTF-8"
-export LC_MONETARY="en_US.UTF-8"
-export LC_NUMERIC="en_US.UTF-8"
-export LC_TIME="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-
 # Aliases
 alias hosts1="sed -i '\'''\'' '\''s/127\.0\.0\.1/127\.0\.0\.2/g'\'' ~/.ssh/known_hosts"
 alias hosts2="sed -i '\'''\'' '\''s/127\.0\.0\.2/127\.0\.0\.1/g'\'' ~/.ssh/known_hosts"
@@ -34,10 +23,6 @@ export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
 echo '
 # Loaders
-loaddocker() {
-  eval $(docker-machine env prl-dev)
-}
-
 loadtheos() {
   export DEVELOPER_DIR=/opt/theos/toolchain/Xcode-11.7.app
 }
